85204474e243be65e35bf109b4c013d579830591
VJHARAN <haranvj1997@gmail.com>
2025-01-20 12:15:20 +0530

    modified fibonacci.py

--- Basic Programs/fibonacci.py
+++ Basic Programs/fibonacci.py
@@ -1,14 +1,13 @@
-num=int(input("Enter the number: "))
-lst=[]
-if num<3:
-    if num==1:
-        lst+=[0]
-    elif num==2:
-        lst+=[0,1]
-    print(f"{num}th fibonacci number= ",lst[-1])
-else:
-    lst+=[0,1]
-    for i in range(2,num+1):
-        lst=lst+[lst[i-1]+lst[i-2]]
-print(lst)
-print(f"{num}th fibonacci number= ",lst[-1])
+def f(num):
+    fib=[]
+    for i in range (num):
+        if i<=1:
+            fib=fib+[i]
+            print(fib[i])
+        else:
+           fib=fib+[fib[i-1]+fib[i-2]]
+           print(fib[i], end=" ")
+
+num=int(input("Enter no of terms:"))
+print(f"Fibonacci upto {num} terms: ")
+f(num)
